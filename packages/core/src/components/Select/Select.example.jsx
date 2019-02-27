43dd4ce0ed5e4b6281895219d792ae435fa3fd65
line47 <scott.weber@adhocteam.us>
2019-02-27 16:56:16 -0500

    Move select to it's own folder. Remove check and radio examples.

--- packages/core/src/components/Select/Select.example.jsx
+++ packages/core/src/components/Select/Select.example.jsx
@@ -4,6 +4,9 @@ import Select from './Select';
 
 ReactDOM.render(
   <div>
+    <label className="ds-c-label" htmlFor="options">
+      Default select
+    </label>
     <Select defaultValue="2" name="select-demo">
       <option value="1">Option 1</option>
       <option value="2">Option 2</option>
@@ -13,12 +16,16 @@ ReactDOM.render(
       <option value="6">Option 6</option>
       <option value="7">Option 7</option>
     </Select>
-    <h4>Small size modifier</h4>
+    <label className="ds-c-label" htmlFor="options">
+      Small select
+    </label>
     <Select defaultValue="Jr." size="small" name="small-select-demo">
       <option value="Jr.">Jr.</option>
       <option value="Sr.">Sr.</option>
     </Select>
-    <h4>Medium size modifier</h4>
+    <label className="ds-c-label" htmlFor="options">
+      Medium select
+    </label>
     <Select defaultValue="2" size="medium" name="medium-select-demo">
       <option value="1">Option 1</option>
       <option value="2">Option 2</option>
